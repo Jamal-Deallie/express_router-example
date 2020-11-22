b96a7cbb8a2fb27b74b909053d21e017f5aa59c9
Jamal DeAllie <deallie1985@gmail.com>
2020-11-21 19:07:21 -0500

    first commit

---
+++ routes/campsiteRouter.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const campsiteRouter = express.Router();
+
+campsiteRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsites: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+})
+.get('/:campsitesId', (req, res) => {
+    res.end(`Will send details of the campsites: ${req.params.campsitesId} to you`);
+  })
+  
+  .post('/:campsitesId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /promotion/${req.params.campsitesId}`);
+  })
+  
+  .put('/:campsitesId', (req, res) => {
+    res.write(`Updating the campsites: ${req.params.campsitesId}\n`);
+    res.end(`Will update the campsites ${req.body.name}
+        with description: ${req.body.description}`);
+  })
+  
+  .delete('/:campsitesId', (req, res) => {
+    res.end(`Deleting campsites ${req.params.campsitesId}`);
+  });
+  
+
+module.exports = campsiteRouter;
